c257c1be61ab446bfe6581a299e3f4a3e1ba893d
shiyaam <shiyaamsunderms@gmail.com>
2023-06-05 19:00:12 +0530

    removed the dynamic cookie getting part

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,5 @@
-import { Main } from './components/Main';
-import { cookies } from 'next/headers';
-
-
-async function getSpotifyToken() {
-  // const clientID = process.env.SPOTIFY_CLIENT_ID || "";
-  // const clientSecret = process.env.SPOTIFY_CLIENT_SECRET || "";
-  // const res = await fetch('https://accounts.spotify.com/api/token', {
-  //   method: "POST",
-  //   headers: {
-  //     "Content-Type": "application/x-www-form-urlencoded"
-  //   },
-  //   body: new URLSearchParams({
-  //     grant_type: "client_credentials",
-  //     client_id: clientID,
-  //     client_secret: clientSecret,
-  //   })
-  // })
-  //
-  // return res.json();
-
-
-
-}
-
+import { Main } from "./components/Main";
 
 export default async function Home() {
-  const cookieStore = cookies();
-  const token = cookieStore.get('access_token');
-  return <Main token={token?.value || ""} />
+  return <Main />;
 }
